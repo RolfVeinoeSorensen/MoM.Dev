c8ed12bcad0039cf0f2f9446c5fa19a0c4512641
Rolf Veinø Sørensen <rolf@veinoe.dk>
2016-06-01 13:07:10 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    rimraf = require('rimraf');
+
+gulp.task('remove-node-folder', function (cb) {
+    rimraf('./node_modules', cb);
+});

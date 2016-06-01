7769448ccac3c1591e38d0bffc2b9b53aa41704a
Rolf Veinø Sørensen <rolf@veinoe.dk>
2016-06-01 18:49:29 +0200

    add npm install to install script

--- install.ps1
+++ install.ps1
@@ -25,12 +25,37 @@ function CloneOrPullRepository([string] $name)
     }
     catch
     {
-        $exceptionMessage = $_.Exception.Message
+        $exceptionMessage = $_.Exception.Message;
         Write-Host "Failed to download '$name': $exceptionMessage"
     }
 }
+
+function InstallNpmPackages()
+{
+    try
+    {        
+        $ArgumentList = 'install"';
+        Start-Process npm -ArgumentList $ArgumentList -Wait;
+        #npm install
+        Write-Host "Installed NPM Packages";
+    }
+    catch
+    {
+        $exceptionMessage = $_.Exception.Message;
+        Write-Host "Failed to download '$name': $exceptionMessage";
+    }
+}
+
+Write-Host "Begin installing MoM for developers";
+Write-Host "________________________________________"
 Write-Host "Begin installing Repositories needed"
 CloneOrPullRepository -name "MoM"
 CloneOrPullRepository -name "MoM.Modules"
 CloneOrPullRepository -name "MoM.Themes"
 Write-Host "Done installing Repositories needed"
+Write-Host "________________________________________"
+Write-Host "Installing NPM Packages - Please be patient with this step!"
+Write-Host "Scary red tekst may appear but most likely all is fine"
+InstallNpmPackages;
+Write-Host "________________________________________"
+Write-Host "Done installing MoM for developers"
